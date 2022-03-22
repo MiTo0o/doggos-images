b3917625bf97a7e3bf4335c2225a152cdf453af8
MiTo0o <76019114+MiTo0o@users.noreply.github.com>
2022-03-21 18:43:22 -0700

    sheesh

--- UTILITY/compress.js
+++ UTILITY/compress.js
@@ -2,5 +2,8 @@ const sharp = require('sharp');
 
 sharp('../Brownie/images/10.jpg')
   .rotate()
-  .resize(600, 400)
-  .toFile('xd.webp')
+  .resize(600, 400, {
+    fit: sharp.fit.inside,
+    withoutEnlargement: true, 
+  })
+  .toFile('xd.jpg')
