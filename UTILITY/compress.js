b6a52da81f36a488cdce9969de3f251342021b45
MiTo0o <76019114+MiTo0o@users.noreply.github.com>
2022-03-22 12:41:35 -0700

    working script

--- UTILITY/compress.js
+++
@@ -1,9 +0,0 @@
-const sharp = require('sharp');
-
-sharp('../Brownie/images/10.jpg')
-  .rotate()
-  .resize(600, 400, {
-    fit: sharp.fit.inside,
-    withoutEnlargement: true, 
-  })
-  .toFile('xd.jpg')
--- UTILITY/script.js
+++ UTILITY/script.js
@@ -1,5 +1,8 @@
+const sharp = require('sharp');
 const fs = require('fs');
+const path = require('path');
 const sizeOf = require('image-size');
+sharp.cache(false);
 
 const filePaths = {
   Brownie: '../Brownie/images',
@@ -7,31 +10,87 @@ const filePaths = {
   Lucky: '../Lucky/images'
 }
 
+async function resizeFile(imgPath, imageFolderPath) {
+  let buffer = await sharp(imgPath)
+    .rotate()
+    .resize(900, 600, {
+      fit: sharp.fit.inside,
+      withoutEnlargement: true,
+    })
+    .toBuffer();
+  const imageName = path.parse(imgPath).name
+  const outputPath = `${imageFolderPath}/${imageName}.webp`
+  return sharp(buffer).webp().toFile(outputPath);
+}
+
+let resizePromises = []
 for (const dog in filePaths) {
-
   const filePath = filePaths[dog]
-
-  const writeData = {
-    title: dog,
-    imgList: []
-  }
-  
   fs.readdirSync(filePath).forEach(file => {
-    const hostedStaticPath = `https://raw.githubusercontent.com/MiTo0o/doggos-static/main/${dog}/images/${file}`
-    let imageSize = sizeOf(`${filePath}/${file}`);
-
-    const imgInfo = {
-      src: hostedStaticPath,
-      width: 1,
-      height: Math.round(imageSize.height / imageSize.width * 1000) / 1000
-    }
-
-    writeData.imgList.push(imgInfo);
-  })
-  const data = `export const ${dog} = ${JSON.stringify(writeData)};`;
-  fs.writeFile(`./compiledData/${dog}.js`, data, (err) => {
-    if (err) throw err;
-    console.log('complete')
+    const imagePath = `${filePath}/${file}`;
+    const imageFolderPath = filePath
+    resizePromises.push(resizeFile(imagePath, imageFolderPath))
   })
 }
 
+// Run next part after resizing everything
+Promise.all(resizePromises)
+  .then(resizePromisesData => {
+
+    // Delete all file types that arent '.webp'
+    for (const dog in filePaths) {
+      const filePath = filePaths[dog]
+      fs.readdirSync(filePath).forEach(file => {
+        let imagePath = `${filePath}/${file}`;
+        let imageExt = path.parse(imagePath).ext;
+        if (imageExt !== '.webp') {
+          fs.unlinkSync(imagePath)
+        }
+      })
+    }
+
+    for (const dog in filePaths) {
+      // rename files incrementing from 1
+
+      const filePath = filePaths[dog]
+      let pathsArray = fs.readdirSync(filePath)
+      pathsArray.sort((a, b) => {
+        let numa = +a.split('.')[0]
+        let numb = +b.split('.')[0]
+        return numa - numb
+      })
+
+      pathsArray.forEach((file, index) => {
+        let imagePath = `${filePath}/${file}`;
+        let fileNumber = index + 1
+        let renamedPath = `${filePath}/${fileNumber}.webp`;
+        fs.renameSync(imagePath, renamedPath)
+
+      })
+    }
+
+    // Compile img data (ratio and links) to be used for doggos website
+    for (const dog in filePaths) {
+      const filePath = filePaths[dog]
+    
+      const writeData = {
+        title: dog,
+        imgList: []
+      }
+      
+      fs.readdirSync(filePath).forEach(file => {
+        const hostedStaticPath = `https://raw.githubusercontent.com/MiTo0o/doggos-static/main/${dog}/images/${file}`
+        let imageSize = sizeOf(`${filePath}/${file}`);
+    
+        const imgInfo = {
+          src: hostedStaticPath,
+          width: 1,
+          height: Math.round(imageSize.height / imageSize.width * 1000) / 1000
+        }
+    
+        writeData.imgList.push(imgInfo);
+      })
+      const data = `export const ${dog} = ${JSON.stringify(writeData)};`;
+      fs.writeFileSync(`./compiledData/${dog}.js`, data)
+    }
+  })
